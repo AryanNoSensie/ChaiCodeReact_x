099e034a5a730fe810ecab51261103b94464ea95
AryanNoSensie <officearyan14@gmail.com>
2025-05-29 18:36:33 +0530

    done with both the things now useref

--- bgColor/src/App.jsx
+++ bgColor/src/App.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function App() {
-  const [bgColor, setBgColor] = useState('grey');
-  return (
-<div className='w-full h-screen' style={{ backgroundColor: bgColor }}>
+// function App() {
+//   const [bgColor, setBgColor] = useState('grey');
+//   return (
+// <div className='w-full h-screen' style={{ backgroundColor: bgColor }}>
 
-  <div className='fixed flex flex-wrap  justify-center bottom-12 inset-x-0 px-2' >
-        <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 rounded-2xl py-3'>
-          <button onClick={()=>setBgColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style = { { backgroundColor: 'red' }} 
-            >red</button>
-            <button onClick={()=>setBgColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style = { { backgroundColor: 'blue' }} 
-            >blue</button>
-            <button onClick={()=>setBgColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style = { { backgroundColor: 'green' }} 
-            >green</button>
-        </div>
+//   <div className='fixed flex flex-wrap  justify-center bottom-12 inset-x-0 px-2' >
+//         <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 rounded-2xl py-3'>
+//           <button onClick={()=>setBgColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style = { { backgroundColor: 'red' }} 
+//             >red</button>
+//             <button onClick={()=>setBgColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style = { { backgroundColor: 'blue' }} 
+//             >blue</button>
+//             <button onClick={()=>setBgColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style = { { backgroundColor: 'green' }} 
+//             >green</button>
+//         </div>
         
-  </div>
+//   </div>
     
-</div>
-  )
-} 
+// </div>
+//   )
+// } 
 
-export default App
+// export default App
  
--- clockTimer/src/App.jsx
+++ clockTimer/src/App.jsx
@@ -2,3 +2,54 @@
 
 //i am building a clock timer , which unmounts the timer and coditional render but the timer starts where it left off 
 
+import {useState, useEffect} from 'react'
+
+function  App(){
+
+
+      const [Timer  , setTimer] = useState(0)
+      const [Visible , setVisible ]  = useState(true);
+      
+
+
+
+
+      useEffect(function(){
+       const toggele = setInterval(() => {
+           setVisible(c=>!c)
+
+       },5000)
+
+       return () => clearInterval(toggele)
+       },[])
+
+     
+
+
+
+      useEffect(function(){
+if (!Visible){return } 
+
+       let clock =  setInterval(() => {
+            setTimer(c =>c+1)
+        }, 1000);
+          
+
+        return ()=> clearInterval(clock)
+          
+    },[Visible])
+    
+
+
+
+    return(
+        <div>
+            <h3>time is </h3>
+            <h3>{Visible ? Timer : "not visible"}</h3>
+
+
+        </div>
+    )
+}
+
+export default App
